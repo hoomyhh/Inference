3ae33ce10927aa88d256fcfb358140ef1642d014
Hoomaan Maskan <91242901+hoomyhh@users.noreply.github.com>
2021-12-20 09:13:52 +0100

    Update CL2.R

--- CL2.R
+++ CL2.R
@@ -1,9 +1,9 @@
 library(readxl)
 setwd("C:/Users/homa0012/Desktop/PhD_Hoomaan/Teaching/ComputerLab2/")
-smoking <- read_excel("smoking.xls")
+smoking <- read.csv("smoking2.csv")
 set.seed (20)
 attach ( smoking )
-SmokYes = split( smoking , Smoke )$Yes ## community of smokers ( Population )
+SmokYes = split( smoking , smoke )$Yes ## community of smokers ( Population )
 data1 = SmokYes [ sample (dim(SmokYes)[1] , 50, replace = FALSE ),]
 #set.seed (45)
 #data2 = SmokYes [ sample (dim( SmokYes )[1] , 50, replace = FALSE ),]
@@ -16,15 +16,15 @@ summary(data1)
 
 samplesize <- 50
 attach ( data1 )
-female_smokers = split(data1,Sex)$"Female"
-male_smokers = split(data1,Sex)$"Male"
+female_smokers = split(data1,gender)$"Female"
+male_smokers = split(data1,gender)$"Male"
 
 female_smokers2 = as.data.frame(female_smokers[,1])
 male_smokers2 = as.data.frame(male_smokers[,1]) 
 
 female_proportion <- length(female_smokers2[,1])/samplesize
 male_proportion <- length(male_smokers2[,1])/samplesize
-gender_proportion = 
+
 
 barplot(c(female_proportion,male_proportion),ylab ="Proportion", xlab="Gender",names.arg = c("Female","Male"),axisnames = TRUE,axis.lty = 3,col = c("BLUE","RED"))
 
@@ -73,15 +73,15 @@ legend("topright",c("Married","Separated","Divorced","Widowed","Single"), fill= 
 boxplot(female_smokers$Age,male_smokers$Age,main = "Female and Male Smokers", at = c(1,2),names = c("Female","Male"),col = c("orange","red"))
 
 
-Average_Smoking_Age= mean(data1$Age)
-var_data = 1/49*sum((data1$Age-Average_Smoking_Age)**2)
+Average_Smoking_Age= mean(data1$age)
+var_data = 1/49*sum((data1$age-Average_Smoking_Age)**2)
 Loweralpha=-1.65
 
 
 LowerBound = Loweralpha * sqrt(var_data/50)+Average_Smoking_Age
 
 
-Average_Smoking_Female= mean(female_smokers$Age)
+Average_Smoking_Female= mean(female_smokers$age)
 
 #install.packages("car")
 #x= rnorm(100)
@@ -89,8 +89,8 @@ Average_Smoking_Female= mean(female_smokers$Age)
 
 
 #Question 2
-Week_average = mean(data1$`Amount Weekends`+data1$`Amount Weekdays`)
-var_data_week = 1/49*sum((data1$`Amount Weekends`+data1$`Amount Weekdays`-Week_average)**2)
+Week_average = mean(data1$amt_weekends+data1$amt_weekdays)
+var_data_week = 1/49*sum((data1$amt_weekends+data1$amt_weekdays-Week_average)**2)
 Upperalpha = -1.65
 UpperBound  = Week_average-sqrt(var_data_week/50)*Upperalpha
 
